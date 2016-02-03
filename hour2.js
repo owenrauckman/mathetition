ab2ae851dc8617d682dfd0bfa21d855e1c16539a
Owen Rauckman <owenrauckman@Owens-MBP.kc.surewest.net>
2016-02-02 19:32:17 -0600

    adds js files

---
+++ hour2.js
@@ -0,0 +1,47 @@
+var teams = [
+  ["No Name", 17],
+  ["Burls", 8],
+  ["Batman", 3],
+  ["Do Math", 4],
+  ["Wobbling Penguins", 9],
+  ["Deez Bubbles", 9]
+];
+
+//blank array to fill
+var sortedTeams = [];
+
+//sorts teams by score
+sortedTeams = teams.sort(function(a, b) {
+    return a[1] - b[1];
+});
+//sort high to low instead
+sortedTeams = sortedTeams.reverse();
+
+
+
+$(document).ready(function(){
+
+  $('#how-to').click(function(){
+    $('.container-points ul').slideToggle();
+  });
+
+  for(var i = 0; i < sortedTeams.length; i++){
+    $('.flex-container').append(
+
+      "<div class='flex-item'>" +
+        "<div class='score'>" +
+          "<h1>" + sortedTeams[i][1] + "</h1>" +
+        "</div>" +
+        "<div class='team-name'>" +
+          "<div class='place'>" +
+            "<p>" + (i+1) + "</p>" +
+          "</div>" +
+          "<div class='name'>" +
+            sortedTeams[i][0] +
+          "</div>" +
+        "</div>" +
+      "</div>"
+
+    );
+  }
+});
